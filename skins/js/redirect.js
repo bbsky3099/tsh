7732ab2194adbd2086f2138d2d2bd00ed90b5882
bbsky3099 <q.jc.cool@163.com>
2024-09-22 17:34:24 +0800

    update

--- skins/js/redirect.js
+++ skins/js/redirect.js
@@ -1,5 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    if (document.referrer !== 'https://bbsky3099.github.io/tsh/') {
-        window.location.href = 'index.html';
-    }
-});
+
